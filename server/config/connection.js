2514ff9495e79c3466860e29345ada4384e8c50b
mcnairjm19 <mcnairjm1993@gmail.com>
2022-04-19 11:57:14 -0400

    added path for mongodb

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-    process.env.MONGODB_URI || 'mongodb://localhost',
+    process.env.MONGODB_URI || 'mongodb://localhost:27017/farm-app-crop-planner',
     {
         useNewUrlParser: true,
         useUnifiedTopology: true,
