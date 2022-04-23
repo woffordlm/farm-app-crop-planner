98dd19538497152a59b78bb7eb92e280eca12e51
reinholz36 <dreinholz28@gmail.com>
2022-04-23 17:25:43 -0400

    merge

--- client/src/App.js
+++ client/src/App.js
@@ -8,23 +8,10 @@ import {
   createHttpLink,
 } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
-<<<<<<< HEAD
-
-import { motion, AnimatePresence } from "framer-motion";
-
-
-import Home from "./pages/Home";
-import Header from "./Components/Header";
-import Sidebar from "./Components/Sidebar";
-import Addplanting from './Components/Addplanting/index';
-import PageTabs from "./Components/Tabs";
-
-=======
 import Home from "./pages/Home";
 import Header from "./Components/Header";
 import Sidebar from "./Components/Sidebar";
 // import { QUERY_CROPREFERENCE } from "./utils/queries";
->>>>>>> develop-lucius
 
 const httpLink = createHttpLink({
   uri: "/graphql",
@@ -46,14 +33,8 @@ const client = new ApolloClient({
 });
 
 function App() {
-<<<<<<< HEAD
-  const [modalOpen, setModalOpen] = useState(false);
-  const close = () => setModalOpen(false);
-  const open = () => setModalOpen(true);
-=======
  
 
->>>>>>> develop-lucius
   return (
     <ApolloProvider client={client}>
       <Router>
@@ -65,19 +46,7 @@ function App() {
               <Route exact path="/" component={Home} />
             </Routes>
           </div>
-<<<<<<< HEAD
-          <PageTabs/>
-
-          <Sidebar>
-              
-          </Sidebar>
-
-          
-        
-
-=======
           <Sidebar />
->>>>>>> develop-lucius
         </div>
       </Router>
     </ApolloProvider>
