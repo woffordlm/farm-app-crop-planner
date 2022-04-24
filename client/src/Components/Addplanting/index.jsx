573e75048bb88731c1726c1d6e96d987d34c2067
reinholz36 <dreinholz28@gmail.com>
2022-04-24 19:03:00 -0400

    added dropdown name select

--- client/src/Components/Addplanting/index.jsx
+++ client/src/Components/Addplanting/index.jsx
@@ -29,7 +29,7 @@ const dropIn = {
 };
 
 const Addplanting = ({ handleClose,modalOpen,data, text }) => {
-console.log('data:', data)
+console.log('data.allCrops:', data.allCrops)
 
 
 
@@ -51,12 +51,10 @@ console.log('data:', data)
                 <h2>{"What vegetable do you want to plant ? "}
                    
                     <select className="close-button">
-                        {console.log()}
-                        <option defaultValue={"Select Plant"}>Select Plant</option>
-                        <option value="grapefruit">Grapefruit</option>
-                        <option value="lime">Lime</option>
-                        <option value="coconut">Coconut</option>
-                        <option value="mango">Mango</option>
+                    <option defaultValue={"Select Plant"}>Select Plant</option>
+                        {data.allCrops && data.allCrops.map(data => (
+                            <option value={data.name} key={data._id}>{data.name}</option>
+                        ))}      
                     </select>
                 </h2>
                     </label>
