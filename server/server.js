ba2388ef7e59a720cf21a00e35a6abdf013e7074
woffordlm <lukewofford@gmail.com>
2022-04-28 08:34:32 -0400

    added static path for photos

--- server/server.js
+++ server/server.js
@@ -26,6 +26,10 @@ startServer();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Serve up static assets
+app.use('/images', express.static(path.join(__dirname, '../client/photos')));
+
+
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join*(__dirname, '../client/build')));
 }
