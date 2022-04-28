e7569c544ffc54ae62cc89d8b342def051851589
woffordlm <lukewofford@gmail.com>
2022-04-28 09:53:04 -0400

    server change

--- server/server.js
+++ server/server.js
@@ -31,7 +31,7 @@ app.use('/images', express.static(path.join(__dirname, '../client/photos')));
 
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join*(__dirname, '../client/build')));
+    app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
 app.get('*', (req, res) => {
